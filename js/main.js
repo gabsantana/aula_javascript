9a6682e885c74db07604286c0506ae0b2164791d
gabsantana <gabriel.m.santana@gmail.com>
2020-04-30 11:03:32 +0100

    Fim de aula.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function botao(){
+    alert("Obrigado por clicar");
+}
+
+/*
+function botao(){
+    document.getElementById("agradecimento").innerHTML="Obrigado por clicar";
+}
+
+function 
+
+function redirecionar(){
+    window.open("www.google.pt");
+    // window.location.href ="www.google.pt";
+} */
+
+//alert(soma(5,10));
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+
+/*
+var count;
+for (count=0;count<=5;count++){
+    alert(count)
+};
+*/
+
+/*
+var count=0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count = count +1;
+};
+*/
+
+/*
+var idade = prompt("qual é a sua idade");
+if ( idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
